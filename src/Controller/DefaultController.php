9dbb70362803bb4b1d1cd29ba036a4ebf287184a
Philip Verbist <philip.verbist@gmail.com>
2017-06-06 03:16:32 +0200

    updated default action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,70 @@
 namespace Drupal\hosting_control_panel\Controller;
 
 class DefaultController {
-    public function hello() {
+    public function index() {
+        defined('YII_DEBUG') or define('YII_DEBUG', true);
+        defined('YII_ENV') or define('YII_ENV', 'dev');
+
+//        require(__DIR__ . '/../../../../../../vendor/autoload.php');
+        require(__DIR__ . '/../../../../../../vendor/yiisoft/yii2/Yii.php');
+
+        $config = $config = [
+            'id' => 'basic',
+            'basePath' => dirname(__DIR__ . '/../../../../../../vendor/xsonline/hosting-control-panel/config'),
+            'bootstrap' => ['log'],
+            'components' => [
+                'request' => [
+                    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                    'cookieValidationKey' => 'so-bLQoWlm1IIiChzihcjtzMZnsbSQ0M',
+                ],
+                'cache' => [
+                    'class' => 'yii\caching\FileCache',
+                ],
+                'user' => [
+                    'identityClass' => 'app\models\User',
+                    'enableAutoLogin' => true,
+                ],
+                'errorHandler' => [
+                    'errorAction' => 'site/error',
+                ],
+                'mailer' => [
+                    'class' => 'yii\swiftmailer\Mailer',
+                    // send all mails to a file by default. You have to set
+                    // 'useFileTransport' to false and configure a transport
+                    // for the mailer to send real emails.
+                    'useFileTransport' => true,
+                ],
+                'log' => [
+                    'traceLevel' => YII_DEBUG ? 3 : 0,
+                    'targets' => [
+                        [
+                            'class' => 'yii\log\FileTarget',
+                            'levels' => ['error', 'warning'],
+                        ],
+                    ],
+                ],
+                'db' => [
+                    'class' => 'yii\db\Connection',
+                    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+                    'username' => 'root',
+                    'password' => '',
+                    'charset' => 'utf8',
+                ],
+                /*
+                'urlManager' => [
+                    'enablePrettyUrl' => true,
+                    'showScriptName' => false,
+                    'rules' => [
+                    ],
+                ],
+                */
+            ],
+        ];//require(__DIR__ . '/../config/web.php');
+
+        (new \verbi\yii2Drupal8Application\Application($config))->run();
+
+        
+        
         return array(
                 '#title' => 'Hosting',
                 '#theme' => 'item_list',
