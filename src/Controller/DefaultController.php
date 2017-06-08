198adf830395c1cab07598864ad8c5b64d835f0c
Philip Verbist <philip.verbist@gmail.com>
2017-06-08 20:19:35 +0200

    getting appropriate response

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\hosting_control_panel\Controller;
 
 class DefaultController {
+
     public function index() {
         defined('YII_DEBUG') or define('YII_DEBUG', true);
         defined('YII_ENV') or define('YII_ENV', 'dev');
@@ -13,68 +14,26 @@ class DefaultController {
         $config = $config = [
             'id' => 'basic',
             'basePath' => dirname(__DIR__ . '/../../../../../../vendor/xsonline/hosting-control-panel/config'),
-            'bootstrap' => ['log'],
-            'components' => [
-                'request' => [
-                    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-                    'cookieValidationKey' => 'so-bLQoWlm1IIiChzihcjtzMZnsbSQ0M',
-                ],
-                'cache' => [
-                    'class' => 'yii\caching\FileCache',
-                ],
-                'user' => [
-                    'identityClass' => 'app\models\User',
-                    'enableAutoLogin' => true,
-                ],
-                'errorHandler' => [
-                    'errorAction' => 'site/error',
-                ],
-                'mailer' => [
-                    'class' => 'yii\swiftmailer\Mailer',
-                    // send all mails to a file by default. You have to set
-                    // 'useFileTransport' to false and configure a transport
-                    // for the mailer to send real emails.
-                    'useFileTransport' => true,
-                ],
-                'log' => [
-                    'traceLevel' => YII_DEBUG ? 3 : 0,
-                    'targets' => [
-                        [
-                            'class' => 'yii\log\FileTarget',
-                            'levels' => ['error', 'warning'],
-                        ],
-                    ],
-                ],
-                'db' => [
-                    'class' => 'yii\db\Connection',
-                    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-                    'username' => 'root',
-                    'password' => '',
-                    'charset' => 'utf8',
-                ],
-                /*
-                'urlManager' => [
-                    'enablePrettyUrl' => true,
-                    'showScriptName' => false,
-                    'rules' => [
-                    ],
-                ],
-                */
+            'vendorPath' => realpath(__DIR__ . '/../../../../../../vendor'),
+            'aliases' => [
+                '@bower' => '@vendor/bower-asset',
+                '@npm' => '@vendor/npm-asset',
             ],
-        ];//require(__DIR__ . '/../config/web.php');
+        ]; //require(__DIR__ . '/../config/web.php');
 
-        (new \verbi\yii2Drupal8Application\Application($config))->run();
+        $response = (new \verbi\yii2Drupal8Application\Application($config))->run();
 
-        
-        
+//        die($response->data);
+
         return array(
-                '#title' => 'Hosting',
-                '#theme' => 'item_list',
-                '#list_type' => 'ul',
-                '#items' => ['item 1', 'item 2'],
-                '#attributes' => ['class' => 'mylist'],
-                '#wrapper_attributes' => ['class' => 'container'],
-//                '#markup' => 'Here is some content.',
-            );
+            '#title' => 'Hosting',
+//            '#theme' => 'item_list',
+//            '#list_type' => 'ul',
+//            '#items' => ['item 1', 'item 2'],
+//            '#attributes' => ['class' => 'mylist'],
+//            '#wrapper_attributes' => ['class' => 'container'],
+            '#markup' => $response->data,
+        );
     }
+
 }
